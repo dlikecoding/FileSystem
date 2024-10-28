1828ff2ce57d23b73a6462f7045bf0ce09f321fc
danish_csc415 <dnguyen@sfsu.edu>
2024-10-28 09:25:46 -0700

    Clean up

--- fsInit.c
+++ fsInit.c
@@ -48,35 +48,6 @@ int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
 	if (vcb->signature == SIGNATURE) {
 		/* TODO: Load free_space_map and root_dir_ptr location HERE */
 
-
-		// printf("FOUND: %ld \n", vcb->signature);
-		// printf("Init : totalFreeBs: %d, extentsLen: %d \n", vcb->fs_st.totalBlocksFree, vcb->fs_st.extentLength);
-		
-
-		// int loadedBlocks = loadFSMap();
-
-		// for (size_t i = 0; i < vcb->fs_st.extentLength; i++) {
-		// 	printf(" ===[%d: %d]=== \n", vcb->free_space_map[i].startLoc, vcb->free_space_map[i].countBlock);
-		// }
-		// printf("AFTER : lastExtentSize: %d", vcb->fs_st.lastExtentSize);
-		
-		// printf("AFTER : totalFreeBs: %d, extentsLen: %d \n", vcb->fs_st.totalBlocksFree, vcb->fs_st.extentLength);
-
-		
-
-		// allocateBlocks(300,0);
-		// allocateBlocks(200,0);
-		// allocateBlocks(400,0);
-		// allocateBlocks(1000,0);
-		// allocateBlocks(500,0);
-		
-		// releaseBlocks(925, 1000);
-		// releaseBlocks(325, 200);
-		
-		// releaseBlocks(25, 300);
-		// releaseBlocks(525, 400);
-
-
 		return 0;
 	}
 
@@ -99,16 +70,6 @@ int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
 
 	//DirectoryEntry location start after FreeSpace loc
 	vcb->root_loc = vcb->root_dir_ptr->block_location.startLoc;
-    
-	// printf("Directory Entry:\n");
-    // printf("  File Name: %s\n", vcb->root_dir_ptr[0].file_name);
-    // printf("  Block Location: %d\n", vcb->root_dir_ptr[0].block_location.startLoc);     
-	// printf("  File Size: %d bytes\n", vcb->root_dir_ptr[0].file_size);
-    // printf("  Is Directory: %d\n", vcb->root_dir_ptr[0].is_directory);
-    // printf("  Is Used: %d\n", vcb->root_dir_ptr[0].is_used);
-    // printf("  Creation Time: %ld\n", vcb->root_dir_ptr[0].creation_time);
-    // printf("  Modification Time: %ld\n", vcb->root_dir_ptr[0].modification_time);
-    // printf("  Access Time: %ld\n", vcb->root_dir_ptr[0].access_time);
 
 	//Write Volumn Control Block back to the disk
 	if (LBAwrite(vcb, 1, 0) < 1) return -1; //write unsuccessed
